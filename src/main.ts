c9581e875a4e877145a39bdee6801f8c06606bff
Denis Listiadi <denislistiadi24@gmail.com>
2025-06-19 19:55:19 +0700

    feat: update .gitignore and tsconfig for generated files and path mappings; enhance main.ts with global interceptors and filters

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,18 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   // Global Validation Pipe
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  // Intercept Response
+  app.useGlobalInterceptors(new TransformInterceptor());
+  app.useGlobalFilters(new AllExceptionsFilter());
 
   app.useStaticAssets(path.join(process.cwd(), 'uploads'), {
     prefix: '/uploads/',
